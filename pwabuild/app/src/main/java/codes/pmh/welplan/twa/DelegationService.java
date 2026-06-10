712c4c00ea4be952b100fc33415f2a925966ccf0
Minhyeok Park <pmh_only@pmh.codes>
2026-06-10 20:47:49 +0900

    feat(pwabuild): add generated TWA project

---
+++ pwabuild/app/src/main/java/codes/pmh/welplan/twa/DelegationService.java
@@ -0,0 +1,14 @@
+package codes.pmh.welplan.twa;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
